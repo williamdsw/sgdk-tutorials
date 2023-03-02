f2b7616327b15764f394b332106419377471d1f9
William Santos <williamdsw@outlook.com>
2023-03-02 19:36:00 -0300

    :ok_hand: How to Display Backgrounds on the Sega Genesis & Mega Drive - Beginners Game Dev Tutorial (SGDK)

--- pigsy/template-project/src/main.c
+++ pigsy/template-project/src/main.c
@@ -1,9 +1,20 @@
 
 #include <genesis.h>
+#include <resources.h>
 
 int main()
 {
-    VDP_drawText("Welcome to the Pigsy SGDK template!!", 2, 13);
+    u16 ind = TILE_USER_INDEX;
+
+    // Use PAL0 for background images
+    PAL_setPalette(PAL0, background.palette->data, DMA);
+    VDP_drawImageEx(BG_B, &background, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, ind), 0, 0, FALSE, TRUE);
+    ind += background.tileset->numTile;
+
+    // Use PAL1 for foreground images
+    PAL_setPalette(PAL1, foreground.palette->data, DMA);
+    VDP_drawImageEx(BG_A, &foreground, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, ind), 0, 0, FALSE, TRUE);
+    ind += foreground.tileset->numTile;
 
     while (1)
     {
