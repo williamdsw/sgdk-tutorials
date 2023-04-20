c7cf2cc5f8a60dac1b498a2258d99fa89f4d7983
William Santos <williamdsw@outlook.com>
2023-04-20 17:46:00 -0300

    Image

---
+++ samples/basics/image/res/resources.h
@@ -0,0 +1,7 @@
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const Image logo_aplib;
+extern const Image logo_ucomp;
+
+#endif // _RES_RESOURCES_H_
---
+++ samples/basics/image/src/main.c
@@ -0,0 +1,49 @@
+
+#include <genesis.h>
+#include <resources.h>
+
+// 0: soft reset
+// 1: hard reset
+int main(u16 resetType)
+{
+    u16 ind;
+
+    // disable interrupt when accessing VDP
+    SYS_disableInts();
+
+    VDP_setScreenWidth320();
+
+    // set all palette to black
+    PAL_setColors(0, palette_black, 64, DMA);
+
+    // load background tilesets in VRAM
+    ind = TILE_USER_INDEX;
+    int idx1 = ind;
+    VDP_loadTileSet(logo_aplib.tileset, ind, DMA);
+    ind += logo_aplib.tileset->numTile;
+    int idx2 = ind;
+    VDP_loadTileSet(logo_ucomp.tileset, ind, DMA);
+    ind += logo_ucomp.tileset->numTile;
+
+    // This one is not packed
+    TileMap *utmap = logo_ucomp.tilemap;
+
+    // Unpack the packed tilemap
+    TileMap *ctmap = unpackTileMap(logo_aplib.tilemap, NULL);
+
+    // draw backgrounds
+    VDP_setTileMapEx(BG_A, ctmap, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, idx1), 0, 0, 0, 0, 30, 12, CPU);
+    VDP_setTileMapEx(BG_A, utmap, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, idx2), 0, 12, 0, 0, 30, 12, CPU);
+
+    PAL_setColors(0, logo_aplib.palette->data, 16, DMA);
+    PAL_setColors(16, logo_ucomp.palette->data, 32, DMA);
+
+    SYS_enableInts();
+
+    while (TRUE)
+    {
+        SYS_doVBlankProcess();
+    }
+
+    return 0;
+}
